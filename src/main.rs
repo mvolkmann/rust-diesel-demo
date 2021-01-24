465aa0a163b11afc7858fbf76af526964564c025
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-01-23 20:30:23 -0600

    minor update

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dbg!(id);
 
     //update_dog(&conn, id, "Oscar Wilde", "German Shorthaired Pointer")?;
-    update_name(&conn, id + 1, "Oscar Wilde")?;
+    update_name(&conn, id, "Oscar Wilde")?;
 
     report_dogs(&conn);
 
